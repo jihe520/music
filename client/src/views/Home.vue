3278caf985f3f6df65e09ee703aa58af35049346
jihe520 <3265832331@qq.com>
2024-12-24 15:51:39 +0800

    a

--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -1,20 +1,10 @@
 <script setup>
 import { ref,onMounted } from 'vue'
-import {getTestList} from '@/apis/testAPI'
 
-const testList = ref([])
-
-// 测试接口
-onMounted(async () => {
-  testList.value = await getTestList()
-  console.log(testList.value)
-})
 
 </script>
 <template>
-  <ul>
-    <li v-for="item in testList" :key="item.id">{{ item.name }}</li>
-  </ul>
+
 </template>
 
 <style scoped>
